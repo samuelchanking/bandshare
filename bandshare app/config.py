ff6a57759b76e742cba102c7a9e9f520bb74b851
samuelchanking <tksamchan@gmail.com>
2025-09-12 13:06:37 +0100

    Update config.py

--- bandshare app/config.py
+++ bandshare app/config.py
@@ -2,9 +2,9 @@
 
 # Your Soundcharts API credentials
 # It's recommended to load these from environment variables for better security
-APP_ID = 'MANCHESTER_696DCD6E'  # Replace with your actual App ID
-API_KEY = '11ed17a6cf25afa4' # Replace with your actual API Key
+APP_ID = ''  # Replace with your actual App ID
+API_KEY = '' # Replace with your actual API Key
 
 # MongoDB connection details
-MONGO_URI = 'mongodb://localhost:27017/'
+MONGO_URI = ''
 DB_NAME = 'soundcharts7'
